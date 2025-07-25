28fa878edb55df64c1bacea1f828b7ef04605899
DeaglanBartlett <47668431+DeaglanBartlett@users.noreply.github.com>
2025-07-25 12:54:49 +0100

    Update notes for fit_literature

--- literature_fits/fit_literature.py
+++ literature_fits/fit_literature.py
@@ -215,5 +215,20 @@ if __name__ == "__main__":
 """
 QUESTIONS:
     - Why did Laura cut the max. wavelength at 1 micron?
+        - Match SDSS filters
+        - (We care least about this part of the curve)
     - Why did Laura cut the minimum wavelength at 0.12 microns for the 4-parameter fit?
+        - Prevent tracing other components
+        - Ionising photos are attempted to be fitted by the 4-parameter fit
+
+    - Ensure data cut to make sure we don't get the weird LOSs where observer near dust-free regions?
+        - See Laura's code
+
+    - LOS is fixed relative to the box and not the galaxy, so this is not a property one would include in a fit
+
+    IOB/PCA -> Functional form -> Do fit of free parameters with this code -> Can you predict those free parameters from physical properties?
+    Simple and pretty!
+
+    Plot ratio of fluxes - not just the attenuation curve
+    - This is what Laura did in her paper
 """
